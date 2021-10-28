7d8434a2b8fd86f5eba323024b629a7723c345b1
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-28 13:27:23 -0400

    fix: use canonicalized path to avoid detection issues when inside notes/cards folders

--- src/cards.rs
+++ src/cards.rs
@@ -30,33 +30,35 @@ pub fn get_cards(path: &str, algorithm: &str) -> Vec<DirEntry> {
     WalkDir::new(path)
         .into_iter()
         .filter_map(|entry_result| match entry_result {
-            Ok(entry) => {
-                if entry
-                    .path()
-                    .components()
-                    .find(|c| c == &Component::Normal(OsStr::new("cards")))
-                    .is_some()
-                {
-                    match entry.path().extension() {
-                        Some(extension_option) => match extension_option.to_str() {
-                            Some(extension) => {
-                                if extension == "md" {
-                                    match review_time(entry.path(), algorithm) {
-                                        true => Some(entry),
-                                        false => None,
+            Ok(entry) => match entry.path().canonicalize() {
+                Ok(cp) => {
+                    if cp
+                        .components()
+                        .find(|c| c == &Component::Normal(OsStr::new("cards")))
+                        .is_some()
+                    {
+                        match entry.path().extension() {
+                            Some(extension_option) => match extension_option.to_str() {
+                                Some(extension) => {
+                                    if extension == "md" {
+                                        match review_time(entry.path(), algorithm) {
+                                            true => Some(entry),
+                                            false => None,
+                                        }
+                                    } else {
+                                        None
                                     }
-                                } else {
-                                    None
                                 }
-                            }
+                                _ => None,
+                            },
                             _ => None,
-                        },
-                        _ => None,
+                        }
+                    } else {
+                        None
                     }
-                } else {
-                    None
                 }
-            }
+                Err(_) => None,
+            },
             Err(_) => None,
         })
         .into_iter()
--- src/notes.rs
+++ src/notes.rs
@@ -9,34 +9,36 @@ pub fn get_notes(path: &str, all: bool) -> Vec<DirEntry> {
     WalkDir::new(path)
         .into_iter()
         .filter_map(|entry_result| match entry_result {
-            Ok(entry) => {
-                if entry
-                    .path()
-                    .components()
-                    .find(|c| c == &Component::Normal(OsStr::new("notes")))
-                    .is_some()
-                {
-                    match entry.path().extension() {
-                        Some(extension_option) => match extension_option.to_str() {
-                            Some(extension) => {
-                                if extension == "md" {
-                                    if all || !spaced(entry.path()).unwrap() {
-                                        Some(entry)
+            Ok(entry) => match entry.path().canonicalize() {
+                Ok(cp) => {
+                    if cp
+                        .components()
+                        .find(|c| c == &Component::Normal(OsStr::new("notes")))
+                        .is_some()
+                    {
+                        match entry.path().extension() {
+                            Some(extension_option) => match extension_option.to_str() {
+                                Some(extension) => {
+                                    if extension == "md" {
+                                        if all || !spaced(entry.path()).unwrap() {
+                                            Some(entry)
+                                        } else {
+                                            None
+                                        }
                                     } else {
                                         None
                                     }
-                                } else {
-                                    None
                                 }
-                            }
+                                _ => None,
+                            },
                             _ => None,
-                        },
-                        _ => None,
+                        }
+                    } else {
+                        None
                     }
-                } else {
-                    None
                 }
-            }
+                Err(_) => None,
+            },
             Err(_) => None,
         })
         .into_iter()
