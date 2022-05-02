20ac50b211d2847c60c0e5099bdca246ba3d880c
Matthew Toohey <contact@mtoohey.com>
2022-05-02 14:27:46 -0400

    build(flake): fix issues flagged by `nix flake check`

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           channel = "nightly";
           sha256 = "m+Yg171wVTSr4Q04fe5KY3fL6RRmk855j/e0kqObW2M=";
         };
-        rust = rustChannel.rust;
+        inherit (rustChannel) rust;
 
         naersk-lib = naersk.lib."${system}".override {
           cargo = rust;
@@ -28,15 +28,14 @@
         };
       in
       rec {
-        packages.spaced = naersk-lib.buildPackage {
+        packages.default = naersk-lib.buildPackage {
           pname = "spaced";
           root = ./.;
 
           nativeBuildInputs = [ pkgs.sqlite ];
         };
-        defaultPackage = packages.spaced;
 
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           nativeBuildInputs = [ rust pkgs.sqlite pkgs.rust-analyzer pkgs.pandoc ];
           shellHook = ''
             export RUST_SRC_PATH="${rustChannel.rust-src}/lib/rustlib/src/rust/library"
